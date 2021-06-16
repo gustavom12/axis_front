f162cb539d808f086750d8b79e38133c0cd5c0b1
julian mercado <gustavo.n.mercado2@gmail.com>
2021-06-16 16:31:24 -0300

    css changes

--- src/components/homeLogged/studentHome/QuizzesRecomend/recomendedQuizzes.tsx
+++ src/components/homeLogged/studentHome/QuizzesRecomend/recomendedQuizzes.tsx
@@ -11,45 +11,53 @@ function RecomendedQuizzes() {
       {loading ? <div className="flex"> <Loader /> </div> :
         <div className="flex flex-column scrollY" style={{ maxWidth: "100%", position: "relative" }}>
           {data?.getQuizes?.map((quiz: any, i: number) =>
-            <div className="recomendQuizCard mb-4" key={i}>
-              <div className="d-flex">
-                <img src={quiz?.imagePresentation} className="presentation" alt="" />
-                <div>
-                  <h6 className="fw-bold text-serif2 mb-1"> {quiz?.title} </h6>
-                  <div className="">
-                    <span className="text-serif2 mx-1">Dificulty:</span>
-                    <span className={
-                      `text-serif2 text-capitalize mx-1
+            <div style={{
+              background: "#eaf0f3",
+              width: "80%",
+              padding: " 15px",
+              borderRadius: "10px",
+              margin: "0 auto"}} className="recomendQuizCard mb-4" key={i}>
+          <div className="d-flex">
+            <img style={{height: "50px",
+    width: "50px",
+    borderRadius: "10px",
+    marginRight: "20px"}} src={quiz?.imagePresentation} className="presentation" alt="" />
+            <div>
+              <h6 className="fw-bold text-serif2 mb-1"> {quiz?.title} </h6>
+              <div className="">
+                <span className="text-serif2 mx-1">Dificulty:</span>
+                <span className={
+                  `text-serif2 text-capitalize mx-1
                           ${quiz.dificulty === "easy" ? "text-success" :
-                        quiz.dificulty === "normal" ? "text-main" :
-                          "text-danger"}`}
-                    >
-                      {quiz?.dificulty}
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <div className="flex">
-                <button className="btn text-white btn-lightblue mx-auto w-75 mt-3 fw-bold text-serif2">
-                  Hacer Quiz
-                  <span className="mx-1">
-                    (
-                    {
-                      quiz?.dificulty === "normal" ?
-                        quiz.pages.length * 7
-                        : quiz?.dificulty === "easy" ?
-                          quiz.pages.length * 5
-                          : quiz.pages.length * 10
-                    }
-                    )EXP
-                  </span>
-                </button>
+                    quiz.dificulty === "normal" ? "text-main" :
+                      "text-danger"}`}
+                >
+                  {quiz?.dificulty}
+                </span>
               </div>
             </div>
-          )}
+          </div>
+          <div className="flex">
+            <button className="btn text-white btn-lightblue mx-auto w-75 mt-3 fw-bold text-serif2">
+              Hacer Quiz
+              <span className="mx-1">
+                (
+                {
+                  quiz?.dificulty === "normal" ?
+                    quiz.pages.length * 7
+                    : quiz?.dificulty === "easy" ?
+                      quiz.pages.length * 5
+                      : quiz.pages.length * 10
+                }
+                )EXP
+              </span>
+            </button>
+          </div>
         </div>
+          )}
+    </div>
       }
-    </section>
+    </section >
   )
 }
 export default RecomendedQuizzes
