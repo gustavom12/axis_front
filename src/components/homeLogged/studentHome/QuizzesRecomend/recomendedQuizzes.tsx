25e64e8b6cb7804f6cafcc2cda335a5854356811
julian mercado <gustavo.n.mercado2@gmail.com>
2021-06-16 14:35:05 -0300

    x

--- src/components/homeLogged/studentHome/QuizzesRecomend/recomendedQuizzes.tsx
+++ src/components/homeLogged/studentHome/QuizzesRecomend/recomendedQuizzes.tsx
@@ -1,47 +1,53 @@
 import { useQuery } from "@apollo/client";
 import React, { } from "react";
 import QuizQueries from "../../../../graphqueries/quiz";
-// import SwiperCore, { } from 'swiper';
-// import { Swiper, SwiperSlide, } from 'swiper/react';
-// Import Swiper styles
-// import 'swiper/swiper.scss';
-// import './recomendedQuizzes.sass'
 import Loader from "../../../loader/loader";
 function RecomendedQuizzes() {
   const { data, loading } = useQuery(QuizQueries.getQuizes, { variables: { limit: 5 } })
   console.log(data?.getQuizes[0])
-  // SwiperCore.use([]);
   return (
-    <section className="mt-4 w-100 notDone misNotas">
-      <h4 className="fw-bold text-serif"> Quizzes for you </h4>
+    <section className="mt-4 w-100 quizzes notDone">
+      <h4 className="text-serif2 mb-3 title"> Quizzes for you </h4>
       {loading ? <div className="flex"> <Loader /> </div> :
-        <h1 className="fw-bold text-center mt-5"> Sección aún no disponible </h1>
-        // <div className="flex" style={{ maxWidth: "100%", position: "relative" }}>
-        //   <Swiper
-        //     spaceBetween={10}
-        //     slidesPerView={"auto"}
-        //     slidesPerGroup={1}
-        //     // centeredSlides={true}
-        //     loop={true}
-        //     style={{ width: "100%", position: "absolute", top: "0", left: "0" }}
-        //   >
-        //     {data?.getQuizes?.map((quiz: any, i: number) =>
-        //       <SwiperSlide className="" key={i} style={{ maxWidth: "100%" }}>
-        //         <div className="recomendQuizCard">
-        //           <div className="d-flex">
-        //             <img src={quiz?.imagePresentation} className="presentation" alt="" />
-        //             <div>
-        //               <h6 className="fw-bold text-serif2 m-0"> {quiz?.title} </h6>
-        //               <span className="text-serif2">Dificulty:</span>
-        //               <span className={`text-serif`}> {quiz?.dificulty} </span>
-        //             </div>
-        //           </div>
-        //         <button className="btn"> {quiz?.pages.length} </button>
-        //         </div>
-        //       </SwiperSlide>
-        //     )}
-        //   </Swiper>
-        // </div>
+        <div className="flex flex-column scrollY" style={{ maxWidth: "100%", position: "relative" }}>
+          {data?.getQuizes?.map((quiz: any, i: number) =>
+            <div className="recomendQuizCard mb-4" key={i}>
+              <div className="d-flex">
+                <img src={quiz?.imagePresentation} className="presentation" alt="" />
+                <div>
+                  <h6 className="fw-bold text-serif2 mb-1"> {quiz?.title} </h6>
+                  <div className="">
+                    <span className="text-serif2 mx-1">Dificulty:</span>
+                    <span className={
+                      `text-serif2 text-capitalize mx-1
+                          ${quiz.dificulty === "easy" ? "text-success" :
+                        quiz.dificulty === "normal" ? "text-main" :
+                          "text-danger"}`}
+                    >
+                      {quiz?.dificulty}
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="flex">
+                <button className="btn text-white btn-lightblue mx-auto w-75 mt-3 fw-bold text-serif2">
+                  Hacer Quiz
+                  <span className="mx-1">
+                    (
+                    {
+                      quiz?.dificulty === "normal" ?
+                        quiz.pages.length * 7
+                        : quiz?.dificulty === "easy" ?
+                          quiz.pages.length * 5
+                          : quiz.pages.length * 10
+                    }
+                    )EXP
+                  </span>
+                </button>
+              </div>
+            </div>
+          )}
+        </div>
       }
     </section>
   )
