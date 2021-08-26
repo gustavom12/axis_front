23757d6194deeefdca86d57a279e45694f933df9
julian mercado <gustavo.n.mercado2@gmail.com>
2021-08-25 21:55:35 -0300

    x

--- src/components/Units/Unit1/Unit1.tsx
+++ src/components/Units/Unit1/Unit1.tsx
@@ -2,6 +2,7 @@ import React, { } from 'react';
 import { Route, Switch } from 'react-router';
 import { Link } from 'react-router-dom';
 import DefiniteArticle from './DefiniteArticle';
+import Prepositions from './prepositions';
 import Pronouns from './Pronouns';
 import './Unit1.sass'
 const Unit1 = ({ match }: { match: any }) => {
@@ -13,10 +14,12 @@ const Unit1 = ({ match }: { match: any }) => {
         <div className="themes flex flex-column mb-5">
           <Link className="theme fw-bold text-white fs-4 w-75" to="/units/1/pronouns">Pronombres</Link>
           <Link className="theme bg-primary fw-bold text-white fs-4 w-75" to="/units/1/definite_article">Artículo determinado</Link>
+          <Link className="theme bg-success fw-bold text-white fs-4 w-75" to="/units/1/prepositions">Preposiciones</Link>
         </div>
       <Switch>
         <Route path={`${match.path}/pronouns`} component={Pronouns} exact={true} />
         <Route path={`${match.path}/definite_article`} component={DefiniteArticle} exact={true} />
+        <Route path={`${match.path}/prepositions`} component={Prepositions} exact={true} />
       </Switch>
     </div>
   )
---
+++ src/components/Units/Unit1/prepositions.tsx
@@ -0,0 +1,31 @@
+import React, { } from "react";
+function Prepositions() {
+  return (
+    <div className="px-5">
+      <h2 className="text-serif2 text-center fs-1 fw-bold"> Prepositions of place </h2>
+      <article className="fs-5 text-center">
+        <span className="fw-bold">
+          Las preposiciones de lugar se colocan detrás del verbo principal, que suele ser el verbo “to be” (estar, ser)
+        </span>
+        en cualquiera de los tiempos pasados, presentes o futuros y en sus formas tanto simples como compuestas.
+      </article>
+      <article className="fs-5 text-center my-3">
+        <span className="fw-bold">
+          Las preposiciones de lugar más comunes son:
+        </span>
+      </article>
+      <div className="flex">
+        <img className="w-75" src="https://i.ibb.co/bPJzwY8/xwda.jpg" alt="" />
+      </div>
+      <article className="fs-5 text-center my-3">
+        <span className="fw-bold">
+          Todas las preposiciones:
+        </span>
+      </article>
+      <div className="flex my-3">
+        <img src="https://i.ibb.co/cxNGVGv/much.jpg" alt="" className="w-75" />
+      </div>
+    </div>
+  )
+}
+export default Prepositions
