7efcc37baad0a543b54bc984f0d5fe7029f0b5b5
julian mercado <gustavo.n.mercado2@gmail.com>
2021-08-30 21:51:01 -0300

    unit 1 changes

--- src/components/Units/Unit1/prepositions.tsx
+++ src/components/Units/Unit1/prepositions.tsx
@@ -19,11 +19,75 @@ function Prepositions() {
       </div>
       <article className="fs-5 text-center my-3">
         <span className="fw-bold">
-          Todas las preposiciones:
+          Todas las preposiciones de lugar:
         </span>
       </article>
       <div className="flex my-3">
         <img src="https://i.ibb.co/cxNGVGv/much.jpg" alt="" className="w-75" />
+      </div>
+      <div className="fs-5 text-center">
+        <h2 className="text-center fw-bold mt-5"> Preposiciones en oraciones </h2>
+        <article className="my-3">
+          <span className="fw-bold ">Nota:</span> Las preposiciones siempre van seguidas por un sustantivo, no un verbo
+        </article>
+        <h2 className="text-center fw-bold "> IN - AT - ON </h2>
+        <article className="">
+          Son unas de las preposiciones más comunes que se pueden usar para indicar lugar o tiempo: in, at y on.
+        </article>
+        <h2 className="text-center fw-bold mt-4">IN</h2>
+        <article className="">
+          <span className="fw-bold">Uso (lugar)</span>: Se usa para indicar tanto espacios cerrados como espacios abiertos.
+        </article>
+        <h5 className="text-primary mt-4">
+          <li>  I found your address in the phone book. </li>
+          <li>The cat is in the box</li>
+        </h5>
+        <article className="">
+          <span className="fw-bold">Uso (tiempo)</span>: Lo utilizamos con meses, años, épocas, partes del día y períodos de tiempo.
+        </article>
+        <h5 className="text-primary mt-4">
+          <li>She was born in 1976.</li>
+          <li>I always run in the mornings.</li>
+        </h5>
+
+        <h2 className="mt-5 fw-bold">AT</h2>
+        <article>
+          <span className="fw-bold">Uso (lugar) : Se usa delante de edificios</span> como casas, aeropuertos, universidades,
+          para acontecimientos como reuniones, fiestas, conciertos, deportes, etc., antes de “top”
+          (parte superior), “bottom” (parte inferior), “the end of” (al final de) y detrás de “arrive”
+          (llegar) cuando nos referimos a lugares que no sean ciudades o países.
+        </article>
+        <h5 className="text-primary mt-4">
+          <li>He is at home</li>
+          <li>Her name is at the bottom of the page.</li>
+          <li>She will see him at the theatre</li>
+          <li>I  always visit my sister at work</li>
+        </h5>
+        <article >
+          <span className="fw-bold">Uso (tiempo)</span>: Lo utilizamos delante de la hora y de fiestas.
+        </article>
+        <h5 className="text-primary mt-4">
+          <li>	He runs every morning at 6</li>
+          <li>	I will see them at Christmas.</li>
+        </h5>
+        <h2 className="fw-bold mt-5">ON</h2>
+        <div className="">
+          <span className="fw-bold">Significado:</span> sobre, encima de algo, tocando
+        </div>
+        <article>
+          <span className="fw-bold">Uso (lugar)</span>: Se coloca delante de nombres de lugares con base
+        </article>
+        <h5 className="text-primary mt-4">
+          <li>They have a photograph of Paris on the wall.(</li>
+        </h5>
+        <article>
+          <span className="fw-bold">Uso (tiempo): </span>
+          Lo utilizamos con días de la semana, fechas y fiestas.
+        </article>
+        <h5 className="text-primary mt-4">
+          <li> 	They went to Mexico on the first of May.</li>
+          <li>He runs on Mondays and Fridays</li>
+        </h5>
       </div>
     </div>
   )
